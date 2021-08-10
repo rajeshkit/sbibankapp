a3c6e9ef854d062fd5e75fe283454df3cd15183c
Rajesh Nallusamy <rajesh.kit@gmail.com>
2021-08-10 16:03:45 +0530

    removed sysouts

--- src/main/java/com/revature/controller/CustomerController.java
+++ src/main/java/com/revature/controller/CustomerController.java
@@ -12,17 +12,17 @@ import com.revature.model.Customer;
 public class CustomerController {
 	@GetMapping("/greet")
 	public String welcome() {
-		System.out.println("welcome");
+		
 		return "welcome to revature";
 	}
 	@GetMapping("/greeting")
 	public String invite() {
-		System.out.println("invite");
+		
 		return "greeting to you";
 	}
 	@GetMapping("/customers")
 	public List<Customer> getCustomer() {
-		System.out.println("welocme customer");
+		
 		return Arrays.asList(new Customer(100, "rajesh"),new Customer(101, "vijay"));
 	}
 	@GetMapping("/test")
